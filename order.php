64fce90a104b6a794cf0631750d88ceb284a1a3d
shelirahayu <if23.shelirahayu@mhs.ubpkarawang.ac.id>
2024-11-18 12:51:58 +0700

    Add files via upload

---
+++ order.php
@@ -0,0 +1,21 @@
+<div class="col-lg-9 mt-2">
+  </>
+  <div class="card">
+    <div class="card-header">
+      Order
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">ini adalah bagian Order </h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quasi veritatis consequuntur numquam eaque repellat temporibus ea sint perspiciatis excepturi. Inventore neque iusto delectus fugit quidem, corrupti rem nisi veritatis.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  <!-- End Content -->
+</div>
+<div class="fixed-bottom text-center mb-2">
+  Copyright 2024 Sambal Bakar
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
